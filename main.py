c2c657da13b5c6e738110822e7f1b6b85840aa81
2mol <2mol@users.noreply.github.com>
2022-07-17 07:19:16 +0200

    sse working!

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from typing import Dict
 
+import asyncio
 import phonenumbers
 import requests
 from fastapi import FastAPI, Request, Form
-from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from pydantic import BaseSettings
+from sse_starlette.sse import EventSourceResponse
 
 from pathlib import Path
 
@@ -118,3 +120,31 @@ async def root_post(request: Request, guess: int = Form(), name: str = Form(), n
             "result.html",
             {"request": request, "txt": "wrong answer :("},
         )
+
+
+STREAM_DELAY = 1  # second
+RETRY_TIMEOUT = 15000  # milisecond
+
+@app.get('/stream')
+async def message_stream(request: Request):
+    # def new_messages():
+    #     # Add logic here to check for new messages
+    #     yield 'Hello World'
+    async def event_generator():
+        while True:
+            # If client closes connection, stop sending events
+            if await request.is_disconnected():
+                break
+
+            # Checks for new messages and return them to client if any
+            # if new_messages():
+            yield {
+                "event": "stream",
+                # "id": "message_id",
+                # "retry": RETRY_TIMEOUT,
+                "data": "message_content"
+            }
+
+            await asyncio.sleep(STREAM_DELAY)
+
+    return EventSourceResponse(event_generator())
